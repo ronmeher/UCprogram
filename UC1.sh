dae43d2b970af21ec056d88262d837c7dcaeef22
ronmeher <ronmeher@gmail.com>
2022-04-08 03:44:24 +0530

    Added a%b+c

--- UC1.sh
+++ UC1.sh
@@ -7,7 +7,10 @@ read -p "Enter value of C: " c;
 result=$(((a + b) * c));
 result1=$(((a * b) + c));
 result2=$(((c + b) / a));
+result3=$(((a % b) + c));
 
 echo "a+b*c: " $result
 echo "a*b+c: " $result1
 echo "c+b/a: " $result2
+echo "a%b+c: " $reault3
+
